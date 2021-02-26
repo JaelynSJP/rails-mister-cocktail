b7275022381acc4a152888de1acc9c93f186dddb
JaelynSng <jaelyn.sng@gmail.com>
2021-02-26 11:50:23 +0800

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,42 +8,35 @@
 require 'json'
 require 'open-uri'
 
-# filepath = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-# ingredient_serialized = open(filepath).read
-# ingredients = JSON.parse(ingredient_serialized)
-
-# puts 'Cleaning Ingredient database...'
-# Ingredient.destroy_all
-
-# ingredients['drinks'].each do |ingredient|
-#   drinks_ingredient = Ingredient.create!(name: ingredient['strIngredient1'])
-#   puts "Created #{drinks_ingredient.name}"
-# end
-
-puts 'Cleaning  database...'
+puts 'Cleaning  Cocktail database...'
 Cocktail.destroy_all
+puts 'Cleaning  Ingredient database...'
 Ingredient.destroy_all
+puts 'Cleaning  Dose database...'
 Dose.destroy_all
 
-filepath_cocktail = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a'
+filepath_cocktail = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita'
 cocktail_serialized = open(filepath_cocktail).read
 cocktails = JSON.parse(cocktail_serialized)
 
 cocktails['drinks'].each do |cocktail|
-  puts cocktail['strGlass']
-  cocktail_created = Cocktail.create!(name: cocktail['strGlass'], imageURL: cocktail['strDrinkThumb'])
-
+  cocktail_created = Cocktail.create!(name: cocktail['strDrink'], imageURL: cocktail['strImageSource'])
   ingredient_check = cocktail['strIngredient1']
-  ingredient = if Ingredient.exists?(ingredient_check)
+  # if Ingredient.exists?(name: ingredient_check)
+  #   puts "#{ingredient_check} exist"
+  # else 
+  #   puts "#{ingredient_check} not exist"
+  #   Ingredient.create!(name: ingredient_check)
+  # end
+  ingredient = if Ingredient.exists?(name: ingredient_check)
                  Ingredient.find_by(name: ingredient_check)
                else
-                 puts ingredient_check
                  Ingredient.create!(name: ingredient_check)
                end
 
   dose = Dose.new(description: cocktail['strMeasure1'])
   dose.ingredient = ingredient
-  dose.cocktail = cocktail
+  dose.cocktail = cocktail_created
   dose.save!
 
   puts "Created #{cocktail_created.name}"
