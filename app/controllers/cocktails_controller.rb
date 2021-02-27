6e71c30a6f9c584af53d24c66588b8e11a086c18
JaelynSng <jaelyn.sng@gmail.com>
2021-02-27 14:33:44 +0800

    updated cocktails_path to root_paths

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -13,7 +13,7 @@ class CocktailsController < ApplicationController
   def create
     @cocktail = Cocktail.new(cocktail_params)
     @cocktail.save
-    redirect_to cocktails_path
+    redirect_to root_path
   end
 
   def edit; end
